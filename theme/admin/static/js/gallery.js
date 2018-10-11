dfad93e5eea6dde04cdeef5c6003514bd61f9889
Iqbal Fauzi <iqbalfawz@gmail.com>
2018-10-11 16:20:53 +0700

    Add options to remove the image

--- theme/admin/static/js/gallery.js
+++ theme/admin/static/js/gallery.js
@@ -13,9 +13,12 @@ $(function(){
         el.list.html('');
         for(var i=0; i<images.length; i++){
             var img = images[i];
-            var path = img.path.replace(/(\.[a-zA-Z]+)$/, '_260x260$1');
-            var tx = '<div class="col-md-4 thumb">'
-                   +    '<div class="thumbnail" href="#">'
+            var path = img.path.replace(/(\.[a-zA-Z]+)$/, '_260x146$1');
+            var tx = '<div class="col-md-4">'
+                   +    '<div class="thumbnail">'
+                   +        '<button type="button" class="close" aria-label="Close" title="Remove" data-index="'+i+'">'
+                   +            '<span aria-hidden="true">&times;</span>'
+                   +        '</button>'
                    +        '<img class="img-responsive" src="'+path+'" alt="#">'
                    +        '<div class="caption">'
                    +            '<abbr title="Click to edit" class="image-title" data-index="'+i+'" data-text="'+img.title+'">'
@@ -44,13 +47,37 @@ $(function(){
         });
     });
 
+    el.list.on('click', '.close', function(){
+        var $el = $(this);
+        var idx = $el.data('index');
+
+        bootbox.confirm({
+            title: 'Delete Confirmation',
+            message: 'Are you sure want to remove this image from current gallery?',
+            callback: function(res){
+                if(!res)
+                    return;
+                images.splice(idx,1);
+                el.input.val(JSON.stringify(images));
+                el.input.change();
+            }
+        })
+    });
+
     el.list.on('click', '.image-title', function(){
         var $el = $(this);
         var idx = $el.data('index');
-        bootbox.prompt('Please set image title', function(val){
-            images[idx].title = val;
-            el.input.val(JSON.stringify(images));
-            el.input.change();
+        bootbox.prompt({
+            title: 'Please set image title',
+            inputType: 'textarea',
+            value: $el.data('text'),
+            callback: function(val){
+                if(val === null)
+                    return;
+                images[idx].title = val;
+                el.input.val(JSON.stringify(images));
+                el.input.change();
+            }
         });
     })
 })
